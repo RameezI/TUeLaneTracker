3173e5661d6006b96cb75e72f6e6774426d7644b
Rameez Ismail <rameez.ismail@nxp.com>
2018-06-08 10:00:02 +0200

    Config committed

--- include/Config.h
+++ include/Config.h
@@ -25,16 +25,17 @@
 
 // General definitions
 
-#define CAMERA_NAME "BUMBLEBEE_DATASET_640x480" /** Target camera and resolution [Loads corresponding calibration file] */
+//#define CAMERA_NAME "LOGITECH_C920_640x480"   /** Target camera and resolution [Loads corresponding calibration file] */
+#define CAMERA_NAME "BUMBLEBEE_640x480"         /** Target camera and resolution [Loads corresponding calibration file] */
 //#define CAMERA_NAME "BRYAN_DATASET_640x480" 	/** Target camera and resolution [Loads corresponding calibration file] */
 #define PROFILER_ENABLED 	        	/**< If defined, the program logs and profiles its execution*/
 #define DISPLAY_GRAPHICS 	        	/**< If defined, the program renders results on to a display */
-//#define DISPLAY_GRAPHICS_DCU          	/**< If defined, the graphics are rendered on the s32v23 DCU*/
+#define DISPLAY_GRAPHICS_DCU          	        /**< If defined, the graphics are rendered on the s32v23 DCU*/
 //#define WRITE_FRAMES_TO_FILE			/**< If defined, the output is saved to a video file */
 
 
 //S32V specific definitions
-//#define S32V2XX				/**< If defined, the program runs s32v-hardware accelerated algorithm */
+#define S32V2XX				        /**< If defined, the program runs s32v-hardware accelerated algorithm */
 #ifdef S32V2XX
  #include "GraphsConfig.h"
 #endif
@@ -52,7 +53,7 @@
 #define VP_RANGE_VER		20   /**< Vertical range of VP on either side of the Horizon Line [pixels] */
 #define VP_RANGE_HOR		300    /**< Horizontal range of VP on either side of center point on the horizon line [pixels] */
 
-#define BUFFER_COUNT		 5    /**<  Provide number of probability frames to buffer for Max-Pooling operations [Temporal Filtering]*/
+#define BUFFER_COUNT		 3    /**<  Provide number of probability frames to buffer for Max-Pooling operations [Temporal Filtering]*/
 
 
 #endif
