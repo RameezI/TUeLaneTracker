ded99cfa062b7b2c0c7107bfc33d21a970f86b7b
Rameez Ismail <rameez.ismail@nxp.com>
2018-07-30 13:59:59 +0200

    store config

--- include/Config.h
+++ include/Config.h
@@ -24,10 +24,10 @@
 
 #define PROFILER_ENABLED	/**< If defined, the program logs and profiles its execution*/
 #define WRITE_FRAME_TO_FILE	/**< If defined, the output is saved to a video file */
-//#define DISPLAY_GRAPHICS_DCU  /**< If defined, the graphics are rendered on the s32v23 DCU*/
+#define DISPLAY_GRAPHICS_DCU  /**< If defined, the graphics are rendered on the s32v23 DCU*/
 
 
-//#define S32V2XX		/**< If defined, the program runs s32v-hardware accelerated algorithm */
+#define S32V2XX		/**< If defined, the program runs s32v-hardware accelerated algorithm */
 #ifdef S32V2XX
  #include "GraphsConfig.h"
 #endif
@@ -44,24 +44,24 @@ namespace LaneTracker
 	//Lane Properties
 	float  lane_avg_width    = 300;
 	float  lane_std_width    = 15;
-	float  lane_min_width    = 200;
+	float  lane_min_width    = 250;
 	float  lane_max_width    = 500;
 	float  lane_marker_width = 15;
 	  
 	//Camera Resolution
 	int    cam_res_v = 480;   		// Vertical   Resolution  [pixels].
-	int    cam_res_h = 640;   		// Horizental Resolution  [pixels].
+	int    cam_res_h = 640;   		// Horizontal Resolution  [pixels].
 
 	// Camera Intrinsic parameters
-	float  cam_fx  = 563.9912133;		//Focal lenght x-axis [pixels]
-	float  cam_fy  = 526.0339806; 		//Focal lenght y-axis [pixels]
+	float  cam_fx  = 563.9912133;		//Focal length x-axis [pixels]
+	float  cam_fy  = 526.0339806; 		//Focal length y-axis [pixels]
 
 	float  cam_cx	= 0;	   		//Principal point x-axis in Image-Center-CS [pixels]
 	float  cam_cy   = 0;			//Principal point y-axis in Image-Center-CS [pixels]
 
 	// Camera Extrinsic parameters
-	float  cam_pitch   = 0;			//Pitch of the camera		
-	float  cam_yaw     = 0;			//Camera yaw a [degrees].
+	float  cam_pitch   = 0;			//Pitch of the camera [degrees].
+	float  cam_yaw     = 0;			//Yaw of the camera [degrees].
 	float  cam_height  = 1.5;		// Camera height from the road [meters].
 	float  cam_lateral_offset = 0;		// Lateral-Offset of the camera from the vehicle symmetry plane[meters].
 
@@ -72,7 +72,7 @@ namespace LaneTracker
 	int    step_lane_filter_cm  = 10; 	/**< Provides bin-size for the 1D lane filters */
 	int    step_vp_filter	    = 10;	/**< Provides bin-size for 2D vanishing-point filter */
 	int    vp_range_ver	    = 20;   	/**< Vertical range of VP on either side of the Horizon Line [pixels] */
-	int    vp_range_hor	    = 300;     /**< Horizontal range of VP on either sides of center point [pixels] */
+	int    vp_range_hor	    = 300;      /**< Horizontal range of VP on either sides of center point [pixels] */
 	
 	int    buffer_count	    = 3;    	/**<  Provide number of frames to buffer[Temporal Filtering]*/
 	bool   display_graphics     = true;	/**< Renders graphics on to a HDMI display */
